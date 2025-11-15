ddaf536a8aacdc0bd583f130a4df5a5e01da3d55
sandeep awad <sandeepawad7@gmail.com>
2025-11-15 15:49:38 +0530

    new_ev_file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ config = load_config()
 train_path = config["paths"]["train_folder"]
 test_path = config["paths"]["test_folder"]
 text_path = config["paths"]["text_file"]
+env_path= config["paths"]["env_file"]
 
 print("Train folder:", train_path)
 print("Test folder:", test_path)
